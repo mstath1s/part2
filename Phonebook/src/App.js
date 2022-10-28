9f60c82626e5ab50085511466467579accbab321
Meletis Macbook <mstath1s@hotmail.com>
2022-10-28 23:39:47 +0300

    2.6 phonebook

--- Phonebook/src/App.js
+++ Phonebook/src/App.js
@@ -1,24 +1,43 @@
 import { useState } from 'react'
 
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas'
+    }
   ]) 
   const [newName, setNewName] = useState('')
-
+  
+  
+  const addName = (event)=>{
+    //prevent submitting html forms
+    event.preventDefault()
+    const nameObject = {
+      name: newName,
+    }
+  
+    setPersons(persons.concat(nameObject))
+    setNewName('')
+     
+  }
+  const handleNameChange = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit = {addName}>
         <div>
-          name: <input />
+          name: <input value = {newName}
+                    onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      {persons.map(person=>{return(<ul>{person.name}</ul>)})}
     </div>
   )
 }
